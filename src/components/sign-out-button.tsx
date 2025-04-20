9484b40f48380eea508a5eee90dc26345d25baa5
putuhema <putuhendramahendra@gmail.com>
2025-04-20 10:44:21 +0800

    chore: change redirect after logout to /

--- src/components/sign-out-button.tsx
+++ src/components/sign-out-button.tsx
@@ -18,7 +18,7 @@ export default function SignOutButton() {
 				await signOut({
 					fetchOptions: {
 						onSuccess: () => {
-							router.push("/sign-in");
+							router.push("/");
 						},
 					},
 				});
