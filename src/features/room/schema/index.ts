8a331a02ad73851bdbf614e6b8b558e79d81b2bf
putuhema <putuhendramahendra@gmail.com>
2025-04-21 09:19:54 +0800

    chore: change max length on room name input

--- src/features/room/schema/index.ts
+++ src/features/room/schema/index.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const roomSchema = z.object({
-	name: z.string().min(4, "Nama ruangan minimal 4 karakter."),
+	name: z.string().min(2, "Nama ruangan minimal 2 karakter."),
 });
 
 export type roomData = z.infer<typeof roomSchema>;
