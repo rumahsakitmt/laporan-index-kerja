c5113d5d5f8b5534532ed8049ab9f0903986c78a
putuhema <putuhendramahendra@gmail.com>
2025-05-06 09:42:05 +0800

    Squash merge ssr into main

--- src/features/report/components/form/report-form.tsx
+++ src/features/report/components/form/report-form.tsx
@@ -3,12 +3,12 @@
 import React, { useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import {
-	Form,
-	FormControl,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
 import { Textarea } from "@/components/ui/textarea";
 import { useForm } from "react-hook-form";
@@ -29,133 +29,134 @@ import FormSkeleton from "./form-skeleton";
 
 
 interface ReportFormProps {
-	reportId?: number;
+  onHandleDialog?: () => void;
+  reportId?: number;
 }
 
-export default function ReportForm({ reportId }: ReportFormProps) {
-	const { data: report, isLoading } = useGetReport(reportId);
-	const { mutate: createReport, isPending: createPending } = useCreateReport();
-	const { mutate: editReport, isPending: editPending } = useEditReport(reportId);
-	const { closeSheet } = useEditSheetStore();
+export default function ReportForm({ reportId, onHandleDialog }: ReportFormProps) {
+  const { data: report, isLoading } = useGetReport(reportId);
+  const { mutate: createReport, isPending: createPending } = useCreateReport();
+  const { mutate: editReport, isPending: editPending } = useEditReport(reportId);
+  const { closeSheet } = useEditSheetStore();
 
-	const [formKey, setFormKey] = React.useState(0);
+  const [formKey, setFormKey] = React.useState(0);
 
-	const form = useForm<reportData>({
-		resolver: zodResolver(reportSchema),
-		defaultValues: {
-			date: new Date(),
-			time: "",
-			room: "",
-			problem: "",
-			task: "",
-			needs: "",
-			status: "",
-			notes: "",
-		},
-	});
+  const form = useForm<reportData>({
+    resolver: zodResolver(reportSchema),
+    defaultValues: {
+      date: new Date(),
+      time: "",
+      room: "",
+      problem: "",
+      task: "",
+      needs: "",
+      status: "",
+      notes: "",
+    },
+  });
 
-	useEffect(() => {
-		if (reportId && report) {
-			form.reset({
-				date: new Date(report.date ?? new Date()),
-				time: report.time ?? "",
-				room: report.room?.id.toString() ?? "",
-				task: report.task?.id.toString() ?? "",
-				problem: report.problem ?? "",
-				needs: report.needs ?? "",
-				status: report.status ?? "",
-				notes: report.notes ?? "",
-			});
+  useEffect(() => {
+    if (reportId && report) {
+      form.reset({
+        date: new Date(report.date ?? new Date()),
+        time: report.time ?? "",
+        room: report.room?.id.toString() ?? "",
+        task: report.task?.id.toString() ?? "",
+        problem: report.problem ?? "",
+        needs: report.needs ?? "",
+        status: report.status ?? "",
+        notes: report.notes ?? "",
+      });
 
-			setTimeout(() => setFormKey((prev) => prev + 1), 0);
-		}
-	}, [reportId, report, form]);
+      setTimeout(() => setFormKey((prev) => prev + 1), 0);
+    }
+  }, [reportId, report, form]);
 
-	const onSubmit = (values: reportData) => {
-		if (reportId) {
-			editReport(values);
-		} else {
-			createReport(values);
-		}
+  const onSubmit = (values: reportData) => {
+    if (reportId) {
+      editReport(values);
+    } else {
+      createReport(values);
+    }
 
-		closeSheet();
+    closeSheet();
 
-		form.reset();
-	};
+    form.reset();
+  };
 
-	if (reportId && isLoading) {
-		return (
-			<FormSkeleton />
-		);
-	}
+  if (reportId && isLoading) {
+    return (
+      <FormSkeleton />
+    );
+  }
 
-	if (reportId && !report) {
-		return <div className="flex justify-center p-4">Report not found</div>;
-	}
+  if (reportId && !report) {
+    return <div className="flex justify-center p-4">Report not found</div>;
+  }
 
-	return (
-		<Form {...form}>
-			<form
-				key={formKey}
-				onSubmit={form.handleSubmit(onSubmit)}
-				className="space-y-4"
-			>
-				<DatePicker form={form} />
-				<FormField
-					control={form.control}
-					name="time"
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Waktu</FormLabel>
-							<FormControl>
-								<TimeRangePicker onChange={field.onChange} />
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
+  return (
+    <Form {...form}>
+      <form
+        key={formKey}
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-4"
+      >
+        <DatePicker form={form} />
+        <FormField
+          control={form.control}
+          name="time"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Waktu</FormLabel>
+              <FormControl>
+                <TimeRangePicker onChange={field.onChange} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-				<RoomSelect form={form} key={`room-${formKey}`} />
-				<TaskSelect form={form} />
+        <RoomSelect form={form} key={`room-${formKey}`} />
+        <TaskSelect form={form} />
 
-				<FormField
-					control={form.control}
-					name="problem"
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Kendala</FormLabel>
-							<FormControl>
-								<Textarea placeholder="Kendala" {...field} />
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
+        <FormField
+          control={form.control}
+          name="problem"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Kendala</FormLabel>
+              <FormControl>
+                <Textarea placeholder="Kendala" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-				<FormField
-					control={form.control}
-					name="needs"
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Kebutuhan</FormLabel>
-							<FormControl>
-								<Textarea placeholder="Kebutuhan" {...field} />
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
+        <FormField
+          control={form.control}
+          name="needs"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Kebutuhan</FormLabel>
+              <FormControl>
+                <Textarea placeholder="Kebutuhan" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-				<StatusSelect form={form} key={`status-${formKey}`} />
+        <StatusSelect form={form} key={`status-${formKey}`} />
 
-				<Button type="submit" className="w-full font-bold uppercase">
-					{
-						editPending || createPending ? <Loader className="mr-2- h-4 w-4 animate-spin" /> :
-							<Send className="mr-2 h-4 w-4" />
-					}
-					{reportId ? "Update Laporan" : "Masukkan Laporan"}
-				</Button>
-			</form>
-		</Form>
-	);
+        <Button onClick={() => onHandleDialog?.()} type="submit" className="w-full font-bold uppercase">
+          {
+            editPending || createPending ? <Loader className="mr-2- h-4 w-4 animate-spin" /> :
+              <Send className="mr-2 h-4 w-4" />
+          }
+          {reportId ? "Update Laporan" : "Masukkan Laporan"}
+        </Button>
+      </form>
+    </Form>
+  );
 }
--- src/features/report/components/report-form-sheet.tsx
+++ src/features/report/components/report-form-sheet.tsx
@@ -2,22 +2,22 @@
 
 import React from "react";
 import {
-	Drawer,
-	DrawerClose,
-	DrawerContent,
-	DrawerFooter,
-	DrawerHeader,
-	DrawerTitle,
-	DrawerTrigger,
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
 } from "@/components/ui/drawer";
 
 import {
-	Sheet,
-	SheetContent,
-	SheetDescription,
-	SheetHeader,
-	SheetTitle,
-	SheetTrigger,
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
 } from "@/components/ui/sheet"
 import { Button } from "@/components/ui/button";
 import ReportForm from "./form/report-form";
@@ -27,63 +27,67 @@ import { useMediaQuery } from "@/hooks/use-media-query";
 import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 
 export default function LaporanIndexContainer() {
-	const [open, setOpen] = React.useState(false);
-	const isDesktop = useMediaQuery("(min-width: 768px)");
+  const [open, setOpen] = React.useState(false);
+  const isDesktop = useMediaQuery("(min-width: 768px)");
 
-	if (isDesktop) {
-		return (
-			<Sheet open={open} onOpenChange={setOpen}>
-				<SheetTrigger asChild>
-					<Button size="sm" className="w-full md:w-max">
-						<NotebookPen />
-						Buat Laporan
-					</Button>
-				</SheetTrigger>
-				<SheetContent>
-					<SheetHeader>
-						<SheetTitle>Form Laporan Index Kerja IT</SheetTitle>
-						<VisuallyHidden>
-							<SheetDescription>
-								Laporan Index Kerja IT adalah laporan yang digunakan untuk mengukur kinerja dan produktivitas tim IT dalam suatu organisasi. Laporan ini mencakup berbagai metrik dan indikator yang relevan untuk mengevaluasi efektivitas tim IT dalam mencapai tujuan bisnis.
-							</SheetDescription>
-						</VisuallyHidden>
-					</SheetHeader>
-					<ScrollArea className="px-8 py-4 overflow-y-auto">
-						<ReportForm />
-					</ScrollArea>
-				</SheetContent>
-			</Sheet>
-		);
-	}
+  const handleCloseDialog = () => {
+    setOpen(false)
+  }
 
-	return (
-		<div>
-			<Drawer>
-				<DrawerTrigger asChild>
-					<Button size="sm" className="w-full md:w-max">
-						<NotebookPen />
-						Buat Laporan
-					</Button>
-				</DrawerTrigger>
-				<DrawerContent className="min-h-[90%]">
-					<DrawerHeader>
-						<DrawerTitle className="text-center">
-							Form Laporan Index Kerja
-						</DrawerTitle>
-					</DrawerHeader>
-					<ScrollArea className="px-8 overflow-y-auto">
-						<ReportForm />
-					</ScrollArea>
-					<DrawerFooter>
-						<DrawerClose asChild>
-							<Button variant="outline">
-								<ArrowLeft />
-								Batal
-							</Button>
-						</DrawerClose>
-					</DrawerFooter>
-				</DrawerContent>
-			</Drawer>
-		</div>
-	);
+  if (isDesktop) {
+    return (
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetTrigger asChild>
+          <Button size="sm" className="w-full md:w-max">
+            <NotebookPen />
+            Buat Laporan
+          </Button>
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Form Laporan Index Kerja IT</SheetTitle>
+            <VisuallyHidden>
+              <SheetDescription>
+                Laporan Index Kerja IT adalah laporan yang digunakan untuk mengukur kinerja dan produktivitas tim IT dalam suatu organisasi. Laporan ini mencakup berbagai metrik dan indikator yang relevan untuk mengevaluasi efektivitas tim IT dalam mencapai tujuan bisnis.
+              </SheetDescription>
+            </VisuallyHidden>
+          </SheetHeader>
+          <ScrollArea className="px-8 py-4 overflow-y-auto">
+            <ReportForm onHandleDialog={handleCloseDialog} />
+          </ScrollArea>
+        </SheetContent>
+      </Sheet>
+    );
+  }
+
+  return (
+    <div>
+      <Drawer>
+        <DrawerTrigger asChild>
+          <Button size="sm" className="w-full md:w-max">
+            <NotebookPen />
+            Buat Laporan
+          </Button>
+        </DrawerTrigger>
+        <DrawerContent className="min-h-[90%]">
+          <DrawerHeader>
+            <DrawerTitle className="text-center">
+              Form Laporan Index Kerja
+            </DrawerTitle>
+          </DrawerHeader>
+          <ScrollArea className="px-8 overflow-y-auto">
+            <ReportForm onHandleDialog={handleCloseDialog} />
+          </ScrollArea>
+          <DrawerFooter>
+            <DrawerClose asChild>
+              <Button variant="outline">
+                <ArrowLeft />
+                Batal
+              </Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </div>
+  );
 }
